e2dac5334178ac51bc0d68e06ca3d43cf54bbeb8
Ahmed A <ahmadameer167@gmail.com>
2026-09-11 19:35:47 +0300

    feat: add Greek letter shorthand resolution and coverage options in CLI

--- uniscript/__init__.py
+++ uniscript/__init__.py
@@ -14,6 +14,19 @@ from uniscript._core import sub, sup
 from uniscript._parser import render
 from uniscript._fallback import FallbackMode
 from uniscript._errors import UniScriptError
+from uniscript._coverage import coverage, check, CoverageReport
+from uniscript._greek import GREEK_SHORTHANDS, resolve_shorthands
 
 __version__ = "0.1.0"
-__all__ = ["sub", "sup", "render", "FallbackMode", "UniScriptError"]
+__all__ = [
+    "sub",
+    "sup",
+    "render",
+    "coverage",
+    "check",
+    "FallbackMode",
+    "CoverageReport",
+    "UniScriptError",
+    "GREEK_SHORTHANDS",
+    "resolve_shorthands",
+]
